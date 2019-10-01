600b1a3dd21f9f62ad24bcfbb75bc0873f73cd2a
jhlee <jh@bejewel.kr>
2019-10-02 00:44:47 +0900

    크롤링 테스트

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const fs = require('fs');
 const lineReader = require('line-reader');
 
 
-//
-// let $ = cheerio.load('<dl><dt><a href="/app/brand/goods_list/herno">HERNO</a><span class="ti_sale"> SALE</span></dt> <dd><a href="/app/brand/goods_list/herno">에르노</a></dd> </dl>');
-// console.log($('a').text());
 const option = {
     method: "GET",
     uri: "https://store.musinsa.com/app/contents/brandshop",
@@ -16,23 +13,49 @@ const option = {
 };
 const tempFile = './test.txt';
 
-request(option, (req, res, body) => {
-    // console.log(body);
-    const $ = cheerio.load(body);
-    $('.brand_li').find('a').each((index, elem) => {
-        const test = {
-            path: elem.attribs.href,
-            name: elem.children[0].data
-        };
+const host = "https://wusinsa.musinsa.com";
 
-        let text = "";
-        if (fs.existsSync(tempFile)) {
-            text = fs.readFileSync(tempFile, 'utf8');
-        }
-        makeFile(text + "\n" + JSON.stringify(test));
-    })
-});
+
+function getBrandList() {
+    request(option, (req, res, body) => {
+        // console.log(body);
+        const $ = cheerio.load(body);
+        $('.brand_li').find('a').each((index, elem) => {
+            const test = {
+                path: elem.attribs.href,
+                name: elem.children[0].data
+            };
+
+            let text = "";
+            if (fs.existsSync(tempFile)) {
+                text = fs.readFileSync(tempFile, 'utf8');
+            }
+            makeFile(text + "\n" + JSON.stringify(test));
+        })
+    });
+}
 
 function makeFile(str) {
-    fs.writeFileSync("./test.txt", str, "utf8");
+    fs.writeFileSync(tempFile, str, "utf8");
 }
+
+function readBrandList() {
+    lineReader.eachLine(tempFile, async (line, last) => {
+        const path = JSON.parse(line).path;
+        const body = await axios.get(host + path);
+        const $ = cheerio.load(body);
+        const test = $('div .list_img').find('a');
+        console.log(test);
+    });
+}
+
+function start() {
+    /*file이 존재 하면 */
+    if (fs.existsSync(tempFile)) {
+        readBrandList()
+    } else {
+        getBrandList();
+    }
+}
+
+start();
