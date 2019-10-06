b442c8b3901fa60ae5d3cc552232365df16fb92c
jhlee <jh@bejewel.kr>
2019-10-06 21:47:39 +0900

    크롤링 테스트

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ const option = {
     uri: "https://store.musinsa.com/app/contents/brandshop",
     headers: {"User-Agent": "Mozilla/5.0"}
 };
+
+
 const tempFile = './test.txt';
 
 const host = "https://wusinsa.musinsa.com";
+const brandListPath = "/app/contents/brandshop";
 
+
+function getUrl(url) {
+    console.log(host + url);
+    return {
+        method: "GET",
+        uri: host + url,
+        headers: {"User-Agent": "Mozilla/5.0"}
+    }
+}
 
 function getBrandList() {
-    request(option, (req, res, body) => {
+    request(getUrl(brandListPath), (req, res, body) => {
         // console.log(body);
         const $ = cheerio.load(body);
         $('.brand_li').find('a').each((index, elem) => {
@@ -40,13 +52,30 @@ function makeFile(str) {
 }
 
 function readBrandList() {
+    console.log("readBrandList");
     lineReader.eachLine(tempFile, async (line, last) => {
         const path = JSON.parse(line).path;
-        const body = await axios.get(host + path);
-        const $ = cheerio.load(body);
-        const test = $('div .list_img').find('a');
-        console.log(test);
+        await request(getUrl(path), (req, res, body) => {
+            const $ = cheerio.load(body);
+            const path = $('.list_img').find('a')[0].attribs.href
+            console.log(path);
+            getBrandInfo(path);
+
+        });
     });
+}
+
+function getBrandInfo(path) {
+    request(getUrl(path), (req, res, body) => {
+        const $ = cheerio.load(body);
+        const path = $('.sallerinfo_detail_section').find('div').each((index, elem) => {
+            const temp = cheerio.load(elem);
+            temp('div').find('dd').each((i, e) => {
+                console.log(e.children[0].data);
+
+            });
+        });
+    })
 }
 
 function start() {
