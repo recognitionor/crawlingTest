16111e1fe8e8c1ad1209d4173ef6a37e462675b1
leejo <jh@bejewel.kr>
2019-10-06 22:15:18 +0900

    data 파싱 해서 csv 파일로 저장 하는것 까지 완료

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const option = {
 };
 
 
-const tempFile = './test.txt';
+const resultFile = './result.csv';
+const brandListFile = './brand_list.txt';
 
 const host = "https://wusinsa.musinsa.com";
 const brandListPath = "/app/contents/brandshop";
@@ -39,28 +40,38 @@ function getBrandList() {
             };
 
             let text = "";
-            if (fs.existsSync(tempFile)) {
-                text = fs.readFileSync(tempFile, 'utf8');
+            if (fs.existsSync(brandListFile)) {
+                text = fs.readFileSync(brandListFile, 'utf8');
             }
-            makeFile(text + "\n" + JSON.stringify(test));
+            makeBrandList(text + "\n" + JSON.stringify(test));
         })
     });
 }
 
-function makeFile(str) {
-    fs.writeFileSync(tempFile, str, "utf8");
+function makeBrandList(str) {
+    fs.writeFileSync(brandListFile, str, "utf8");
+}
+
+function makeBrandInfo(str) {
+    let text = "";
+    if (fs.existsSync(resultFile)) {
+        text = fs.readFileSync(resultFile, 'utf8');
+    }
+    fs.writeFileSync(resultFile, text + "\n" + JSON.stringify(str), "utf8");
 }
 
 function readBrandList() {
-    console.log("readBrandList");
-    lineReader.eachLine(tempFile, async (line, last) => {
+    lineReader.eachLine(brandListFile, async (line, last) => {
         const path = JSON.parse(line).path;
         await request(getUrl(path), (req, res, body) => {
-            const $ = cheerio.load(body);
-            const path = $('.list_img').find('a')[0].attribs.href
-            console.log(path);
-            getBrandInfo(path);
-
+            if (body) {
+                const $ = cheerio.load(body);
+                const target = $('.list_img').find('a')[0];
+                if (target) {
+                    const path = target.attribs.href;
+                    getBrandInfo(path);
+                }
+            }
         });
     });
 }
@@ -68,19 +79,20 @@ function readBrandList() {
 function getBrandInfo(path) {
     request(getUrl(path), (req, res, body) => {
         const $ = cheerio.load(body);
+        let tempStr = "";
         const path = $('.sallerinfo_detail_section').find('div').each((index, elem) => {
             const temp = cheerio.load(elem);
             temp('div').find('dd').each((i, e) => {
-                console.log(e.children[0].data);
-
+                tempStr += e.children[0].data + ", ";
             });
         });
+        makeBrandInfo(tempStr)
     })
 }
 
 function start() {
     /*file이 존재 하면 */
-    if (fs.existsSync(tempFile)) {
+    if (fs.existsSync(brandListFile)) {
         readBrandList()
     } else {
         getBrandList();
